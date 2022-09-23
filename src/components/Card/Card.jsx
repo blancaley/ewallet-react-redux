5e1af854215b1b28be05b8027de08c1f1a777e90
Blanca Ley <blanca.ley@outlook.com>
2022-09-23 14:39:04 +0200

    Show preview of new card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,10 @@
 const Card = ({ cardInfo }) => {
   return (
     <article>
-      <p>CardNumber: {cardInfo.cardNumber}</p>
-      <p>Cardholder Name {cardInfo.userFirstName} {cardInfo.userLastName}</p>
-      <p>Valid thru: {cardInfo.cardMonth}/{cardInfo.cardYear}</p>
-      <i>Vendor: {cardInfo.vendor}</i>
+      <p>CardNumber: {cardInfo?.cardNumber}</p>
+      <p>Cardholder Name {cardInfo?.userFirstName} {cardInfo?.userLastName}</p>
+      <p>Valid thru: {cardInfo?.cardMonth}/{cardInfo?.cardYear}</p>
+      <i>Vendor: {cardInfo?.vendor}</i>
     </article>
   );
 }
--- src/routes/AddCard.jsx
+++ src/routes/AddCard.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
+import Card from "../components/Card/Card";
+import CardForm from "../components/Card/CardForm/CardForm";
+
 const AddCard = () => {
+
+  const [ cardInfo, setCardInfo ] = useState({
+    cardNumber: "XXXXXXXXXXXX",
+    userFirstName: "NEW INACTIVE",
+    userLastName: "lastname",
+    cardMonth: "MM",
+    cardYear: "DD",
+    ccv: "",
+    vendor: "",
+    isActive: false
+  })
+
+  const onChange = e => {
+    setCardInfo({...cardInfo, [e.target.id]: e.target.value})
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(cardInfo)
+  }
+
   return (
     <div>
-      Add Card
+      <h1>Add a new bank card</h1>
+      <h2>New card</h2>
+      <Card cardInfo={cardInfo}/>
+      
+      <CardForm cardInfo={cardInfo} onChange={onChange} handleSubmit={handleSubmit}/>
+
     </div>
   );
 }
- 
+
 export default AddCard;
