06d0abcaa0bee435f01ff7ebaedea0007dcb4f3d
Blanca Ley <blanca.ley@outlook.com>
2022-09-23 11:41:49 +0200

    Display card info from redux

--- src/routes/Cards.jsx
+++ src/routes/Cards.jsx
@@ -1,6 +1,14 @@
+import { getUser } from "../components/Card/cardSlice"
+import { useSelector, useDispatch } from "react-redux";
 import Card from "../components/Card/Card";
+import { Link } from "react-router-dom";
 
 const Cards = () => {
+  const cardInfo = useSelector((state) => state.card);
+
+  const dispatch = useDispatch();
+  dispatch(getUser);
+
   return (
     <div>
       <h1>E-wallet</h1>
